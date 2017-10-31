4c0d7fe3332a9db1894e96aee8a2b2b684ab7bec
Thalison Bauer <thalisonbauer@hotmail.com>
2017-10-31 20:14:12 -0200

    TheEnde :)

--- src/Aula14/main.java
+++ src/Aula14/main.java
@@ -1,12 +1,10 @@
 package Aula14;
 public class main {
     public static void main(String[] args) {
-        int[] num = {20,8,15,6,35,40,7,22,21,99,12};
+        int[] num = {20,8,15,6,35,40,7,22,21,4};
+        int h = num.length / 2;
         int x = num.length;
-        int h = x;
-        int aux, cont,i = 0;
-
-        h= h/2;
+        int aux, cont,i;
 
     while (h > 0) {
         for (i = 0; i < x; i++) {
@@ -31,6 +29,5 @@ public class main {
         System.out.println(num[7]);
         System.out.println(num[8]);
         System.out.println(num[9]);
-        System.out.println(num[10]);
     }
 }
